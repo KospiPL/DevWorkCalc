576e416b8d6b5fd66857f9787e021b5c8654c401
KospiPL <kacper.cudzik11@gmail.com>
2024-04-05 15:00:05 +0200

    Opublikowanie API

--- D.W.C.API/Program.cs
+++ D.W.C.API/Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using D.W.C.API.D.W.C.Service;
 using D.W.C.Lib;
 using D.W.C.Lib.D.W.C.Models;
-using Microsoft.EntityFrameworkCore;
+using System;
 using Microsoft.Extensions.Options;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,34 +26,46 @@ builder.Services.Configure<AzureDevOpsSettings>(
 
 // Register HttpClient
 builder.Services.AddHttpClient();
-builder.Services.AddAutoMapper(typeof(WorkItemProfile));
+
+// Add AutoMapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 // Register AzureDevOpsClient as a service
 builder.Services.AddScoped<AzureDevOpsClient>(sp =>
 {
     var httpClient = sp.GetRequiredService<HttpClient>();
     var settings = sp.GetRequiredService<IOptions<AzureDevOpsSettings>>();
-    return new AzureDevOpsClient(httpClient, settings);
+    return new AzureDevOpsClient(httpClient, settings.Value); // U¿yj settings.Value aby uzyskaæ instancjê AzureDevOpsSettings
 });
 
 // Configure Swagger
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "D.W.C. API", Version = "v1" });
+    // Dodaj komentarze XML do Swagger UI, aby poprawiæ dokumentacjê API.
+    var xmlFile = $"{AppDomain.CurrentDomain.FriendlyName}.xml";
+    var xmlPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
 });
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "D.W.C. API v1");
+        c.RoutePrefix = string.Empty; 
+    });
+//}
 
 app.UseHttpsRedirection();
+
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
