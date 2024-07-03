ec44b2fb72b65f4c62bfc1c6f9f8a4dd7b42fc30
KospiPL <kacper.cudzik11@gmail.com>
2024-07-03 15:47:39 +0200

    DOdanie i poprawki

--- D.W.C.APP/MauiProgram.cs
+++ D.W.C.APP/MauiProgram.cs
@@ -1,4 +1,12 @@
-﻿using Microsoft.Extensions.Logging;
+using D.W.C.APP.Service;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Components.Authorization;
+using Blazored.LocalStorage;
+using Microsoft.Maui.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Radzen;
+using Radzen.Blazor;
+
 
 namespace D.W.C.APP
 {
@@ -14,11 +22,26 @@ namespace D.W.C.APP
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 });
 
+            // Configure Blazor and local services
             builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+
+
+
+            builder.Services.AddBlazoredLocalStorage();
+            // Configure authentication system
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+            builder.Services.AddScoped<MicrosoftAuthService>();
+            builder.Services.AddScoped<AuthenticationService>();
+            builder.Services.AddRadzenComponents();
+            // Register HttpClient
+            builder.Services.AddScoped<HttpClient>();
 
 #if DEBUG
-    		builder.Services.AddBlazorWebViewDeveloperTools();
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
---
+++ D.W.C.APP/Service/TaskService.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+using D.W.C.Lib.D.W.C.Models;
+
+namespace D.W.C.APP.Service
+{
+    public class TaskService
+    {
+        private HttpClient httpClient = new HttpClient();
+
+        public async Task<List<WorkItemDetails>> GetTasksAsync()
+        {
+            var response = await httpClient.GetAsync("https://localhost:7123/api/ItemDet");
+            response.EnsureSuccessStatusCode();
+            var tasks = await response.Content.ReadFromJsonAsync<List<WorkItemDetails>>(); 
+            return tasks;
+        }
+    }
+}
